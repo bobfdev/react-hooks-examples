6d23dc8c95932678ed62238912eafd00c53fe645
Bob F <bobfdev@gmail.com>
2022-01-02 20:49:48 -0600

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import UseRefExample1 from "../components/UseRefExample1";
+
+
+function Home() {
+    return (
+        <div>
+            <UseRefExample1 />
+        </div>
+    )
+}
+
+export default Home
