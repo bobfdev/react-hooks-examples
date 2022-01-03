400ab00518021cf8415c9557ab86be23073f406c
Bob F <bobfdev@gmail.com>
2022-01-02 22:44:58 -0600

    updated files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import UseRefExample1 from "../components/UseRefExample1";
+import UseRefExample2 from "../components/UseRefExample2";
 
 
 function Home() {
     return (
         <div>
             <UseRefExample1 />
+            <UseRefExample2 />
         </div>
     )
 }
