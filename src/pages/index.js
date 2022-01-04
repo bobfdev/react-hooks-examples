774ce19098f20b06c8138f8b8f6b196834f42b38
Bob F <bobfdev@gmail.com>
2022-01-03 23:33:27 -0600

    updated files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import UseRefExample1 from "../components/UseRefExample1";
 import UseRefExample2 from "../components/UseRefExample2";
+import UseRefExample3 from "../components/UseRefExample3";
 
 
 function Home() {
@@ -7,6 +8,7 @@ function Home() {
         <div>
             <UseRefExample1 />
             <UseRefExample2 />
+            <UseRefExample3 />
         </div>
     )
 }
